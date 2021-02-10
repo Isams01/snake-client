2aaf6612ca412639c4b272f1a3e529df181fa184
Isamu Ito <isamsito@gmail.com>
2021-02-10 21:28:04 +0000

    add user input handler

--- play.js
+++ play.js
@@ -4,7 +4,7 @@
  */
 const connect  = require('./client.js');
 console.log('Connecting...');
-connect();
+const conn = connect();
 
 /**
  * Setup User Interface 
@@ -14,6 +14,28 @@ const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput);
   stdin.resume();
   return stdin;
 }
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if(key === '\u001B\u005B\u0044'){
+    console.log('left arrow')
+  }
+  if(key === '\u001B\u005B\u0041'){
+    console.log('up arrow')
+  }
+  if(key === '\u001B\u005B\u0043'){
+    console.log('right arrow')
+  }
+  if(key === '\u001B\u005B\u0042'){
+    console.log('down arrow')
+  }
+  console.log(key.toString());
+}
+conn.on('data', handleUserInput);
+setupInput();
+
