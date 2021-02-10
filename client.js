af5c402d11d769745984519892e86da941e765c2
Isamu Ito <isamsito@gmail.com>
2021-02-10 20:27:54 +0000

    move play.js function to client.js

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541 
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  // retrieve data from server
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  } )
+
+  // confirm connection to server
+  conn.on('connect', () => {
+    console.log('got a connection');
+  })
+
+  return conn;
+}
+
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,31 +1,7 @@
-const net = require('net');
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  conn.on('connect', () => {
-    console.log('got a connection');
-  });
-  return conn;
-}
-
-console.log('Connecting ...');
-let connection = connect();
-
-connection.on('data', (data) => {
-  console.log('Server says: ', data);
-} )
-
-connection.on('connect', () => {
-  console.log('got a connection');
-})
+const connect  = require('./client.js');
+console.log('Connecting...');
+connect();
